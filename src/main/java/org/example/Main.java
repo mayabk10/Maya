ffa71c036f0e49d86a04e0d830ce680926f833ae
oritoledano <oritoledano123@gmail.com>
2024-09-20 12:42:32 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        //tr2
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Hello , world");
+        System.out.println("type a number");
+        int num1 = scanner.nextInt();
+        System.out.println("type a number");
+        int num2 = scanner.nextInt();
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+
+
+
+    }
+}
+
+
